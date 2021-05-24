248917d645fc1f803378185666e97340cee5c8e9
Trevor Suter <Trevorsuter@icloud.com>
2021-05-24 13:35:23 -0600

    areturns 404 if no params given tested and applied

--- tests/test_stackoverflow.py
+++ tests/test_stackoverflow.py
@@ -17,19 +17,35 @@ def client(app):
 
 
 def test_jobs_has_successful_response(client):
-  response = client.get("/api/v1/jobs?location=denver,co&position=software+engineer")
+  response = client.get("/api/v1/jobs?location=denver,co&title=software+engineer")
   assert response.status_code == 200
 
 def test_jobs_has_data_key(client):
-  response = client.get("/api/v1/jobs?location=denver,co&position=software+engineer")
+  response = client.get("/api/v1/jobs?location=denver,co&title=software+engineer")
   body = response.json
   assert 'data' in body.keys()
   
+def test_jobs_have_correct_keys(client):
+  response = client.get("/api/v1/jobs?location=denver,co&title=software+engineer")
+  body = response.json
   data = body['data']
 
-  assert 'id' in data.keys()
-  assert 'type' in data.keys()
-  assert 'attributes' in data.keys()
+  for job in data:
+    assert 'id' in job.keys()
+    assert 'type' in job.keys()
+    assert 'attributes' in job.keys()
 
+    assert 'title' in job['attributes'].keys()
+    assert 'company' in job['attributes'].keys()
+    assert 'category' in job['attributes'].keys()
+    assert 'description' in job['attributes'].keys()
+    assert 'location' in job['attributes'].keys()
+    assert 'publish_date' in job['attributes'].keys()
+    assert 'link' in job['attributes'].keys()
+
+def test_404_status_if_no_params_given(client):
+  response = client.get("api/v1/jobs")
+
+  assert response.status_code == 404
 
   
